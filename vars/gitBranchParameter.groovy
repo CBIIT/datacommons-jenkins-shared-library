1c99b6cd3be5113b36b24c51f34f7403220e3103
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-06-21 13:09:28 -0400

    refactored more

--- vars/gitBranchParameter.groovy
+++ vars/gitBranchParameter.groovy
@@ -1,3 +1,8 @@
 def call(Map gitParams){
-    gitParameter(branch: "", branchFilter: "origin/(.*)", defaultValue: "master", description: "Select Branch or Tag to build", name: "${gitParams.name}", quickFilterEnabled: false, selectedValue: "NONE", sortMode: "NONE", tagFilter: "*", type: "GitParameterDefinition",useRepository: "${gitParams.remoteRepoUrl}")
+    parameters(
+            [
+                    gitParameter(branch: "", branchFilter: "origin/(.*)", defaultValue: "master", description: "Select Branch or Tag to build", name: "${gitParams.name}", quickFilterEnabled: false, selectedValue: "NONE", sortMode: "NONE", tagFilter: "*", type: "GitParameterDefinition",useRepository: "${gitParams.remoteRepoUrl}")
+            ]
+
+    )
 }
