0a809e31608723796fa7a6433374491b7d587f47
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-06-20 21:05:57 -0400

    refactored properties

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,6 +1,10 @@
 def call(Map config=[:],Closure body) {
     node("${config.label}") {
         projectProperties()
+        tools {
+            maven 'maven-3.6.1'
+            jdk 'Default'
+        }
         ansiColor('xterm') {
             timestamps {
 
