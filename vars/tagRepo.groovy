ff62620ab7129efe2327ab5c01f4e0dc489cf18d
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-02 18:20:01 -0400

    added tag capability

--- vars/tagRepo.groovy
+++ vars/tagRepo.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
         env.GIT_TOKEN = sh(script: 'aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token | tr -d \'\\n\'', returnStdout: true)
-        env.GIT_URL = "${config.gitUrl}.replace('https://','')"
+        env.GIT_URL = "${config.gitUrl}".replace('https://','')
         sh """
         cd "${WORKSPACE}/${config.checkoutDirectory}"
         echo "Applying tag ${config.gitTag} to ${config.gitUrl}"
