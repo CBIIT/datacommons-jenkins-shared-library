1566eea0d1e8abc4018c41c3f4eb066da139fa8b
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-02 16:30:33 -0400

    added tag capability

--- vars/tagRepo.groovy
+++ vars/tagRepo.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
 
         sh "cd ${WORKSPACE}/${config.checkoutDirectory}"
-        def gitToken = sh "aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token)"
+        sh "gitToken=\$(aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token)"
 		echo "Applying tag ${config.gitTag} to ${config.gitUrl}"
         echo "${gitToken}"
 //		git config user.email "jenkins@bento-tools.org"
