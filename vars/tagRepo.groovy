4b9292882c535f8102259d7180068462de406877
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-02 15:59:42 -0400

    added tag capability

--- vars/tagRepo.groovy
+++ vars/tagRepo.groovy
@@ -1,12 +1,12 @@
 def call(Map config){
     sh '''
         #!/bin/bash
-        cd ${config.checkoutDirectory}
+        cd "${config.checkoutDirectory}"
         gitToken=${aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token}
 		echo "Applying tag ${config.gitTag} to ${config.gitUrl}"
 		git config user.email "jenkins@bento-tools.org"
 		git config user.name "Bento Jenkins"
-		git tag --no-sign -a ${config.gitTag}-${BUILD_NUMBER} -m "Jenkins tag: ${config.gitTag}-${BUILD_NUMBER}"
-		git push https://${gitToken}:x-oauth-basic@${config.gitUrl} --tags
+		git tag --no-sign -a "${config.gitTag}-${BUILD_NUMBER}" -m "Jenkins tag: ${config.gitTag}-${BUILD_NUMBER}"
+		git push "https://${gitToken}:x-oauth-basic@${config.gitUrl}" --tags
     '''
 }
