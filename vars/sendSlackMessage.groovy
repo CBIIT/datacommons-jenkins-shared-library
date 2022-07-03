1a514bd1ead441cb7a4e88b015def489ec742f58
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-02 21:46:31 -0400

    added secret method

--- vars/sendSlackMessage.groovy
+++ vars/sendSlackMessage.groovy
@@ -20,8 +20,8 @@ def call(Map slackParams) {
             ]
     )
     try {
-        process = [ 'sh', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${slackPayload}' '${slackParams.slackUrl}'" ].execute()
-        //sh "curl -X POST -H 'Content-type: application/json' --data '${slackPayload}'  '${slackParams.slackUrl}'"
+        //process = [ 'sh', '-c', "curl -v -k -X POST -H \"Content-Type: application/json\" -d '${slackPayload}' '${slackParams.slackUrl}'" ].execute()
+        sh "curl -X POST -H 'Content-type: application/json' --data '${slackPayload}'  '${slackParams.slackUrl}'"
     }catch(err){
         println "${err} Slack notify failed"
     }
