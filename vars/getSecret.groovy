2f06ac69e8f0020b16f2ecc277540a7586b8021c
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-02 20:25:38 -0400

    added secret method

--- vars/getSecret.groovy
+++ vars/getSecret.groovy
@@ -1,7 +1,7 @@
 def call(Map config){
     echo "In secret"
     env.SECRET_NAME="${config.secretName}"
-    env.SECRET = sh(script: "aws secretsmanager get-secret-value  --secret-id $SECRET_NAME --region us-east-1 | jq --raw-output .SecretString | jq -r .token | tr -d '\n'", returnStdout: true)
+    env.SECRET = sh(script: "aws secretsmanager get-secret-value  --secret-id \$SECRET_NAME --region us-east-1 | jq --raw-output .SecretString | jq -r .token | tr -d '\n'", returnStdout: true)
     echo "${SECRET}"
     return env.SECRET
 }
