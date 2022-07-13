90c2ed026eba05bd567a1189c6fa0821cc868101
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-13 11:45:42 -0400

    fixing jenkins

--- vars/deployProperties.groovy
+++ vars/deployProperties.groovy
@@ -8,14 +8,7 @@ def call(Map config,Closure body){
                             [
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Choose the environment that this deployment should apply', filterLength: 1, filterable: false, name: 'Environment', randomName: 'choice-parameter-50324845038543063', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["dev"]'], script: [classpath: [], sandbox: true, script: 'return ["dev","qa","stage","prod","perf"]']]],
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: 'ProjectName', randomName: 'choice-parameter-50324845054357731', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["bento"]'], script: [classpath: [], sandbox: true, script: 'return ["bento","cds","gmb","ctdc","c3dc","vote"]']]],
-                                    [$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: "${config.parameterName}", randomName: 'choice-parameter-8744093374048404', referencedParameters: '', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["latest"]'], script: [classpath: [], sandbox: true, script: '''def cmd = "/usr/local/bin/aws ecr list-images --repository-name cds-backend --filter tagStatus=TAGGED --region us-east-1 --output text"
-def ecr_images_json = cmd.execute()
-ecr_images_json.waitFor()
-def ecr_images_list = ecr_images_json.text.readLines().collect { it.substring(it.lastIndexOf("\\t") + 1) }
-ecr_images_list.removeAll([\'latest\'] as Object[])
-ecr_images_list.removeAll { it.contains(\'production\') }
-List tagnames = ecr_images_list.sort()
-return tagnames''']]],
+                                    
                                     body()
                             ]
 
