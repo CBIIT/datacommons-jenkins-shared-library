37994d44baf3b5d2d0b066721327d3dac1555ecf
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-15 19:15:20 -0400

    testing properties

--- vars/deployProperties.groovy
+++ vars/deployProperties.groovy
@@ -8,10 +8,10 @@ def call(Map config){
                             [
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Choose the environment that this deployment should apply', filterLength: 1, filterable: false, name: 'Environment', randomName: 'choice-parameter-5032484503854306', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["dev"]'], script: [classpath: [], sandbox: true, script: 'return ["dev","qa","stage","prod","perf"]']]],
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: 'ProjectName', randomName: 'choice-parameter-5032484505435771', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["bento"]'], script: [classpath: [], sandbox: true, script: 'return ["bento","cds","gmb","ctdc","c3dc","vote"]']]],
-                                    [$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: "${config.parameterName}", randomName: 'choice-parameter-8744093374048404', referencedParameters: '', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return ["latest"]'], script: [classpath: [], sandbox: false, script: """def cmd = '/usr/local/bin/aws ecr list-images --repository-name \${config.repoName} --filter tagStatus=TAGGED --region us-east-1 --output text'
+                                    [$class: 'CascadeChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: "${config.parameterName}", randomName: 'choice-parameter-8744093374048404', referencedParameters: '', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: false, script: 'return ["latest"]'], script: [classpath: [], sandbox: false, script: """def cmd = "/usr/local/bin/aws ecr list-images --repository-name ${config.repoName} --filter tagStatus=TAGGED --region us-east-1 --output text"
 def ecr_images_json = cmd.execute()
 ecr_images_json.waitFor()
-def ecr_images_list = ecr_images_json.text.readLines().collect { it.substring(it.lastIndexOf("\\t") + 1) }
+def ecr_images_list = ecr_images_json.text.readLines().collect { it.substring(it.lastIndexOf("\t") + 1) }
 ecr_images_list.removeAll('latest'] as Object[])
 ecr_images_list.removeAll { it.contains('production') }
 List tagnames = ecr_images_list.sort()
