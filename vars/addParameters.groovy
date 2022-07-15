7ee73c0588cafb89bbb733b3bca4d9328d4c7b71
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-15 17:16:17 -0400

    testing properties

---
+++ vars/addParameters.groovy
@@ -0,0 +1,13 @@
+//This solution comes from https://stackoverflow.com/questions/48952478/use-groovy-to-add-an-additional-parameter-to-a-jenkins-job
+def call(Closure body){
+    existing = currentBuild.rawBuild.parent.properties
+            .findAll { it.value instanceof hudson.model.ParametersDefinitionProperty }
+            .collectMany { it.value.parameterDefinitions }
+    jobParams = [
+            body()
+    ] + existing
+
+    properties([
+            parameters(jobParams)
+    ])
+}
