0ddf08baeabc3f7f19c83db919e70c5644c79d73
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-17 21:29:09 -0400

    testing properties

--- vars/setDeployEnvValues.groovy
+++ vars/setDeployEnvValues.groovy
@@ -1,3 +1,8 @@
-def call(Closure body)
+def call(Closure body) {
+    env.FE_VERSION = "${params.FrontendTag}"
+    env.BE_VERSION = "${params.BackendTag}"
+    env.AUTH_VERSION = "${params.AuthTag}-${BUI"
+    env.PROJECT_NAME = "${params.ProjectName}-${BUILD_NUMBER}"
+    env.FILES_VERSION = "${params.FilesTag}-${BUILD_NUMBER}"
     body()
 }
