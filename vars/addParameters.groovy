c88b82964824dc5e6487fe0ade84e067b7bab2b9
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-23 23:20:34 -0400

    additional properties

--- vars/addParameters.groovy
+++ vars/addParameters.groovy
@@ -2,7 +2,7 @@ def call(Map config){
     existing = currentBuild.rawBuild.parent.properties
             .findAll { it.value instanceof hudson.model.ParametersDefinitionProperty }
             .collectMany { it.value.parameterDefinitions }
-    jobParams = config.additionalParams + existing
+    jobParams = existing + config.additionalParams
     properties([
             parameters(jobParams)
     ])
