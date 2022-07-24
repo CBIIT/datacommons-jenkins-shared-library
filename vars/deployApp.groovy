cdf62473df946548f1d8cb83ff7c8d43473cc81b
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-24 15:04:18 -0400

    additional properties

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -4,7 +4,7 @@ def call(Map config=[:]){
         gitCheckout checkoutDirectory: "icdc-devops", gitUrl: "https://github.com/CBIIT/icdc-devops", gitBranch: "master"
         setEnvValues(type: "deploy"){}
         stage("deploy"){
-            runAnsible playbook: "${config.playbook}", inventory: "${config.inventory}", tier: "${config.tier}", projectName: "${config.projectName}"
+            runAnsible playbook: "${WORKSPACE}/icdc-devops/ansible/${config.playbook}", inventory: "${WORKSPACE}/icdc-devops/ansible/${config.inventory}", tier: "${config.tier}", projectName: "${config.projectName}"
         }
         stage("send slack"){
             notify secretPath: "${config.slackSecretPath}", secretName: "${config.slackSecretName}"
