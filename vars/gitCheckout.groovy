08ce2d207414bfdffb142a991766843340f65a96
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-24 00:15:22 -0400

    additional properties

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,11 +1,5 @@
 def call(Map config) {
-    if ("${config.checkoutDirectory}"){
         dir("${config.checkoutDirectory}"){
             git branch: "${config.gitBranch}", changelog: false, poll: false, url: "${config.gitUrl}"
         }
-    }else{
-        dir("${WORKSPACE}"){
-            git branch: "${config.gitBranch}", changelog: false, poll: false, url: "${config.gitUrl}"
-        }
-    }
 }
