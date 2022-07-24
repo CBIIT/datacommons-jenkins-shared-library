9e9469a2677ef5e0e7ba8917c7e9436a30e09dbc
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-24 15:56:44 -0400

    moving to deployment

--- vars/tagRepo.groovy
+++ vars/tagRepo.groovy
@@ -1,8 +1,14 @@
 def call(Map config) {
         env.GIT_TOKEN = sh(script: 'aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token | tr -d \'\\n\'', returnStdout: true)
         env.GIT_URL = "${config.gitUrl}".replace('https://','')
+        def targetDirectory = ""
+        if ("${config.checkoutDirectory}" == "workspace"){
+                targetDirectory = "${WORKSPACE}"
+        }else{
+                targetDirectory = "${WORKSPACE}/${config.checkoutDirectory}"
+        }
         sh """
-        cd "${WORKSPACE}/${config.checkoutDirectory}"
+        cd "${targetDirectory}"
         echo "Applying tag ${config.gitTag} to ${config.gitUrl}"
         git config user.email "jenkins@bento-tools.org"
         git config user.name "Bento Jenkins"
