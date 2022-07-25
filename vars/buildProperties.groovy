862f6d1cc9b6d85c57aa01566915b83f685b926d
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-24 23:27:40 -0400

    matching ahead

--- vars/buildProperties.groovy
+++ vars/buildProperties.groovy
@@ -1,4 +1,8 @@
 def call(Map config){
+    def defaultBranch = "master"
+    if (config.useCustomBranch == "yes") {
+        defaultBranch = config.customBranch
+    }
     properties(
             [
                     [$class: 'JiraProjectProperty'],
@@ -8,7 +12,7 @@ def call(Map config){
                             [
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Choose the environment that this deployment should apply', filterLength: 1, filterable: false, name: 'Environment', randomName: 'choice-parameter-5032484503854306', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["dev"]'], script: [classpath: [], sandbox: true, script: 'return ["dev","qa","stage","prod","perf"]']]],
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: 'ProjectName', randomName: 'choice-parameter-5032484505435771', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["bento"]'], script: [classpath: [], sandbox: true, script: 'return ["bento","cds","gmb","ctdc","c3dc","vote"]']]],
-                                    gitParameter(branch: "", branchFilter: "origin/(.*)", defaultValue: "master", description: "Select Branch or Tag to build", name: "${config.name}", quickFilterEnabled: false, selectedValue: "NONE", sortMode: "NONE", tagFilter: "*", type: "GitParameterDefinition",useRepository: "${config.remoteRepoUrl}")
+                                    gitParameter(branch: "", branchFilter: "origin/(.*)", defaultValue: "${defaultBranch}", description: "Select Branch or Tag to build", name: "${config.name}", quickFilterEnabled: false, selectedValue: "NONE", sortMode: "NONE", tagFilter: "*", type: "GitParameterDefinition",useRepository: "${config.remoteRepoUrl}")
                             ]
 
                     )
