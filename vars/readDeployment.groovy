081a153a08445abd038e11294307a9e6efddf072
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-07-26 23:34:39 -0400

    adding deployment.yaml

--- vars/readDeployment.groovy
+++ vars/readDeployment.groovy
@@ -1,13 +1,11 @@
 //import groovy.yaml.YamlBuilder
 def call(Map config = [:]){
     stage('read deployments.yaml') {
-        step {
-            script{
                 deployments = readYaml file: config.deploymentFile
                 data.services.each {
                     val -> println val
                 }
             }
-        }
+
     }
-}
+
