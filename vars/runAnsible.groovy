2fcfed3d651a5efbeaa48d93f565d77ba7cee9e0
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-16 10:58:41 -0400

    fixing ansible

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -1,8 +1,10 @@
-def call(Map config){
+def call(Map config = [:]){
     def extraVars = [tier: config.tier, project_name: config.projectName]
 
     if (config.extraVars){
-        println config.extraVars
+        config.extraVars.each{
+            extraVars[it.key] = it.value
+        }
     }
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
         ansiblePlaybook(
