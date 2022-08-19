200ee81868573df017d6bcbd2259e12452576ac1
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-19 17:29:55 -0400

    fixing release

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -1,11 +1,10 @@
 def call(Map config){
     println  config.extraAnsibleVars
-    println "Hello ${config.extraAnsibleVars[0]}"
+
     def extraVars = [tier: config.tier, project_name: config.projectName]
 
     if (config.extraAnsibleVars){
-        println "Hello ${config.extraAnsibleVar}"
-        println "Hello ${config.extraAnsibleVar}"
+        println config.extraAnsibleVars
 
     }
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
