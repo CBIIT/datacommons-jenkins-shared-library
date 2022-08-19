eca3b6d286ac879dfd0f28a96599361ff166da5f
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-19 14:27:34 -0400

    fixing release

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -1,11 +1,12 @@
 def call(Map config = [:]){
-    def extraAnsibleVars = [:]
+    println config.extraAnsibleVars
+    config.extraAnsibleVars.each{
+        println "looping" + "${it}"
+    }
     def extraVars = [tier: config.tier, project_name: config.projectName]
     if (config.extraAnsibleVars){
         extraVars = extraVars.plus(config.extraAnsibleVars)
-        config.extraAnsibleVars.each{
-            println "looping" + "${it}"
-        }
+
     }
     println "Can't read config"
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
