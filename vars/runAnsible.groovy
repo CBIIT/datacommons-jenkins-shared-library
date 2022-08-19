c58b7bcd8ccd457cf81444e8853b770f9f42902a
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-19 16:24:23 -0400

    fixing release

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -3,7 +3,7 @@ def call(Map config){
     def extraVars = [tier: config.tier, project_name: config.projectName]
     if (config.extraAnsibleVars){
         println "Hello ${config.extraAnsibleVar}"
-        println "Hello,${config.extraAnsibleVar[0]})
+        println "Hello ${config.extraAnsibleVar[0]}"
 
     }
     println "Can't read config"
