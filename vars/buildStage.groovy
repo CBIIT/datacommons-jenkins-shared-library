a7302d920ab0d534371c6d2281ebaff35320366d
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-20 17:50:32 -0400

    done with extravars

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -1,8 +1,23 @@
 def call(Map config=[:],Closure body) {
-    node("${config.label}") {
-        ansiColor('xterm') {
-            timestamps {
-                body()
+    if (config.useDockerAgent){
+        node("${config.label}") {
+            ansiColor('xterm') {
+                timestamps {
+                    docker.withRegistry( config.dockerRegistryUrl, config.registryCredentialsId) {
+                        def buildAgent = docker.image(config.agentImage)
+                        buildAgent.inside("--net=host -u root"){
+                            body()
+                        }
+                    }
+                }
+            }
+        }
+    }else{
+        node("${config.label}") {
+            ansiColor('xterm') {
+                timestamps {
+                    body()
+                }
             }
         }
     }
