23781890d3c94b160dac25b367e3e446abd008e8
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-20 18:15:51 -0400

    done with extravars

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -7,7 +7,7 @@ def call(Map config=[:],Closure body) {
                     docker.withRegistry( config.dockerRegistryUrl, config.registryCredentialsId) {
                         def buildAgent = docker.image(config.agentImage)
                         buildAgent.pull()
-                        buildAgent.inside("--net=host -u root"){
+                        buildAgent.inside("--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock"){
                             body()
                         }
                     }
