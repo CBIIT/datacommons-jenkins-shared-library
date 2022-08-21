8b73e21b4e5925c08188495f3e6a7ea40db8df9b
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-20 21:12:52 -0400

    done with extravars

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,32 @@
 def call(Map config) {
-    if ("${config.checkoutDirectory}" == "workspace"){
-            git branch: "${config.gitBranch}", changelog: false, poll: false, url: "${config.gitUrl}"
-    } else {
-        dir("${config.checkoutDirectory}") {
-            git branch: "${config.gitBranch}", changelog: false, poll: false, url: "${config.gitUrl}"
+//    if ("${config.checkoutDirectory}" == "workspace"){
+//            git branch: "${config.gitBranch}", changelog: false, poll: false, url: "${config.gitUrl}"
+//    } else {
+//        dir("${config.checkoutDirectory}") {
+//            git branch: "${config.gitBranch}", changelog: false, poll: false, url: "${config.gitUrl}"
+//        }
+//    }
+    stage("Checkout"){
+        step {
+            if ("${config.checkoutDirectory}" == "workspace"){
+                checkout([
+                        $class: 'GitSCM', branches: [[name: "${config.gitBranch}"]],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        submoduleCfg: [],
+                        userRemoteConfigs: [[url: "${config.gitUrl}"]]
+                ])
+
+            }else {
+                checkout([
+                        $class: 'GitSCM', branches: [[name: "${config.gitBranch}"]],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${config.checkoutDirectory}"]],
+                        submoduleCfg: [],
+                        userRemoteConfigs: [[url: "${config.gitBranch}"]]
+                ])
+            }
         }
     }
 }
+
