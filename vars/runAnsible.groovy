ba8915b00c4f947c22cf0ded0a9f5506a7ed7408
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-24 18:12:22 -0400

    fixing tag issue

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -1,9 +1,8 @@
 def call(Map config){
 
-
     def extraVars = [tier: config.tier, project_name: config.projectName]
 
-    if (config.extraAnsibleVars != ""){
+    if (config.extraAnsibleVars != null){
         def passedVars = config.extraAnsibleVars.tokenize(",").collectEntries {
             it.tokenize(":").with {
                 [(it[0]):it[1]]
