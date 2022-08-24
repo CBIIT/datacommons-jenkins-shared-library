69baf1cca69808688c55deb3d14ce68ed504dfa7
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-24 18:38:16 -0400

    fixing tag issue

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -2,7 +2,7 @@ def call(Map config){
 
     def extraVars = [tier: config.tier, project_name: config.projectName]
 
-    if (config.extraAnsibleVars != ""){
+    if (config.extraAnsibleVars != null){
         def passedVars = config.extraAnsibleVars.tokenize(",").collectEntries {
             it.tokenize(":").with {
                 [(it[0]):it[1]]
@@ -10,6 +10,7 @@ def call(Map config){
         }
         extraVars = extraVars.plus(passedVars)
     }
+    println extraVars
     wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
         ansiblePlaybook(
                 playbook: config.playbook,
