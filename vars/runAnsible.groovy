6d87b60cdb55d17ada79f41b9dbab8b6ba5e22eb
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-08-25 07:57:29 -0400

    fixing tag issue

--- vars/runAnsible.groovy
+++ vars/runAnsible.groovy
@@ -1,8 +1,8 @@
 def call(Map config){
 
     def extraVars = [tier: config.tier, project_name: config.projectName]
-    
-    if (params.AuthEnabled =! null){
+
+    if (params.AuthEnabled != null){
         extraVars = [tier: config.tier, project_name: config.projectName, auth_enabled: params.AuthEnabled ]
     }
 
