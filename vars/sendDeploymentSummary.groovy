c66cd4e5f17cc9420f1e9293b1ce0896920781c5
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-23 22:06:13 -0400

    added summary notification

--- vars/sendDeploymentSummary.groovy
+++ vars/sendDeploymentSummary.groovy
@@ -10,12 +10,12 @@ def call(Map config) {
         services.add([type: "mrkdwn", text: it.value.version])
     }
     def fields = [type: "section",fields: services]
-    def blocks = [blocks: [header,title,fields ]]
+    def blocks = [[header,title,fields ]]
     long epoch = System.currentTimeMillis()/1000
     def BUILD_COLORS = ['SUCCESS': 'good', 'FAILURE': 'danger', 'UNSTABLE': 'danger', 'ABORTED': 'danger']
     def attachments = [[title: config.title,text: config.text,fallback: config.fallbackMessage,footer: config.footerText,ts: epoch,mrkdwn_in: ["footer", "title"],color: "${BUILD_COLORS[currentBuild.currentResult]}"]]
 //    icon_emoji: config.emojiIcon,
-    def slackPayload = JsonOutput.toJson(blocks)
+    def slackPayload = JsonOutput.toJson([blocks: blocks,attachments: attachments])
     try {
         sh "curl -X POST -H 'Content-type: application/json' --data '${slackPayload}'  '${config.slackUrl}'"
     }catch(err){
