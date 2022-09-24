3dea0d005c67e355bf73c503f4f6bcf601b1acbd
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-24 18:29:12 -0400

    added summary notification

--- vars/sendDeploymentSummary.groovy
+++ vars/sendDeploymentSummary.groovy
@@ -10,7 +10,7 @@ def call(Map config) {
         services.add([type: "mrkdwn", text: it.value.version])
     }
     def fields = [type: "section",fields: services]
-    def blocks = [[header,title,fields ]]
+    def blocks = [header,title,fields ]
     long epoch = System.currentTimeMillis()/1000
     def BUILD_COLORS = ['SUCCESS': 'good', 'FAILURE': 'danger', 'UNSTABLE': 'danger', 'ABORTED': 'danger']
     def attachments = [[title: config.title,text: config.text,fallback: config.fallbackMessage,footer: config.footerText,ts: epoch,mrkdwn_in: ["footer", "title"],color: "${BUILD_COLORS[currentBuild.currentResult]}"]]
