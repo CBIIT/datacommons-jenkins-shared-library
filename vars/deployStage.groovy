0c88632c7ffedb97bfb9da2601fb30dd7e3b79bf
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-09-30 21:35:37 -0400

    added node env to agent

--- vars/deployStage.groovy
+++ vars/deployStage.groovy
@@ -6,7 +6,7 @@ def call(Map config=[:],Closure body) {
                     docker.withRegistry( config.dockerRegistryUrl, config.registryCredentialsId) {
                         def buildAgent = docker.image(config.agentImage)
                         buildAgent.pull()
-                        buildAgent.inside("--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock"){
+                        buildAgent.inside("--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock -e NODE_OPTIONS='--max-old-space-size=8192'"){
                             body()
                         }
                     }
