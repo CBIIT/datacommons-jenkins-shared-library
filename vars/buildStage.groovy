fd4ced5e1ba1057fb875ca46064510667f2023c3
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-10-02 18:52:33 -0400

    working on modular dataloader

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -7,15 +7,12 @@ def call(Map config=[:],Closure body) {
                         def buildAgent = docker.image(config.agentImage)
                         buildAgent.pull()
                         withDockerContainer(
-                                args: "--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock",
+                                args: "--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock -e NODE_OPTIONS='--max-old-space-size=8192'",
                                 image: config.agentImage
                         ){
                             body()
                         }
 
-//                        buildAgent.inside("--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock"){
-//                            body()
-//                        }
                     }
                 }
             }
