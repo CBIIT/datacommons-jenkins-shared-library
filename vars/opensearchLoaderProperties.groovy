97db9b17d3f97fd9a654c5b82b88d828ac902389
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-10-05 12:46:51 -0400

    adding opensearch job

--- vars/opensearchLoaderProperties.groovy
+++ vars/opensearchLoaderProperties.groovy
@@ -13,12 +13,12 @@ def call(Map config){
                             [
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', description: 'Choose the environment that this deployment should apply', filterLength: 1, filterable: false, name: 'Environment', randomName: 'choice-parameter-5032484503854306', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["dev"]'], script: [classpath: [], sandbox: true, script: 'return ["dev","qa","stage","prod","perf","local"]']]],
                                     [$class: 'ChoiceParameter', choiceType: 'PT_SINGLE_SELECT', filterLength: 1, filterable: false, name: 'ProjectName', randomName: 'choice-parameter-5032484505435771', script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true, script: 'return ["bento"]'], script: [classpath: [], sandbox: true, script: 'return ["bento","cds","gmb","ctdc","c3dc","icdc"]']]],
-                                    string(defaultValue: "",
-                                            description: 'S3 Bucket',
-                                            name: 'DataBucket'),
-                                    string(defaultValue: "",
-                                            description: 'S3 Folder to load data from',
-                                            name: 'S3Folder'),
+//                                    string(defaultValue: "",
+//                                            description: 'S3 Bucket',
+//                                            name: 'DataBucket'),
+//                                    string(defaultValue: "",
+//                                            description: 'S3 Folder to load data from',
+//                                            name: 'S3Folder'),
                                     extendedChoice(
                                             defaultValue: 'no',
                                             name: 'WipeDB',
