815f970d1ac892c705bcc3d0ebdf4fb9abd456d8
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-10-04 23:49:58 -0400

    adding ssh

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -10,10 +10,12 @@ def call(Map config) {
                         userRemoteConfigs: [[url: "${config.gitUrl}"]]
                 ])
             }else if(config.checkoutSubmodule == "true"){
+                def splitedUrl = "${config.gitUrl}".split("/")
+                def sshUrl = "git@${splitedUrl[2]}:${splitedUrl[3]}/${splitedUrl[4]}"
                 echo "Here"
-                sh "mkdir -p ~/.ssh && touch ~/.ssh/config"
-                sh '''echo "StrictHostKeyChecking no" > ~/.ssh/config'''
-                git credentialsId: 'git-ssh-cred', url: config.gitUrl,branch: config.gitBranch
+//                sh "mkdir -p ~/.ssh && touch ~/.ssh/config"
+//                sh '''echo "StrictHostKeyChecking no" > ~/.ssh/config'''
+                git credentialsId: 'git-ssh-cred', url: sshUrl,branch: config.gitBranch
             }else {
                 checkout([
                         $class: 'GitSCM', branches: [[name: "${config.gitBranch}"]],
