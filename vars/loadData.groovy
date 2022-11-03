09381ad653958ff7a8232dd3b0365b9613f22b0c
cloudprofessionals <mycloudprofessionals@gmail.com>
2022-11-03 15:10:55 -0400

    fixed property file typo

--- vars/loadData.groovy
+++ vars/loadData.groovy
@@ -32,7 +32,7 @@ def call(Map config=[:]){
                                 split_transactions: config.split_transactions,
                                 model_file_1: config.model_file1,
                                 model_file_2: config.model_file2,
-                                property_file: config.property_file,
+                                property_File: config.property_file,
                         ]
                 )
         }
