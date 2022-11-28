fc6bf7bde8bb41e6ba4142c01029d91a7717ffe3
Michael Fleming <michael.fleming@nih.gov>
2022-11-28 13:32:03 -0500

    reverted changes

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -8,7 +8,7 @@ def call(Map config=[:],Closure body) {
 						def nodeMemory = config.nodeMemory ?: "8192"
                         buildAgent.pull()
                         withDockerContainer(
-                                args: "--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock --shm-size=4096m -e NODE_OPTIONS='--max-old-space-size=${nodeMemory}'",
+                                args: "--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock -e NODE_OPTIONS='--max-old-space-size=${nodeMemory}'",
                                 image: config.agentImage
                         ){
                             body()
