fa08e7d3b69382fac39d17fd792a6ac994a64a79
Michael Fleming <michael.fleming@nih.gov>
2022-11-29 09:20:13 -0500

    updated node memory settings

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -5,7 +5,8 @@ def call(Map config=[:],Closure body) {
                 timestamps {
                     docker.withRegistry( config.dockerRegistryUrl, config.registryCredentialsId) {
                         def buildAgent = docker.image(config.agentImage)
-						def nodeMemory = config.nodeMemory ?: "8192"
+						//def nodeMemory = config.nodeMemory ?: "8192"
+						def nodeMemory = config.nodeMemory ?: "4096"
                         buildAgent.pull()
                         withDockerContainer(
                                 args: "--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock -e NODE_OPTIONS='--max-old-space-size=${nodeMemory}'",
