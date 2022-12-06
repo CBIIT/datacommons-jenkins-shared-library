45fa39bd5a473831077268b978e1a8824b1b04ee
Michael Fleming <michael.fleming@nih.gov>
2022-12-06 13:14:52 -0500

    added try catch for docker builds

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -3,18 +3,31 @@ def call(Map config=[:],Closure body) {
         node("${config.label}") {
             ansiColor('xterm') {
                 timestamps {
-                    docker.withRegistry( config.dockerRegistryUrl, config.registryCredentialsId) {
-                        def buildAgent = docker.image(config.agentImage)
-						def nodeMemory = config.nodeMemory ?: "8192"
-                        buildAgent.pull()
-                        withDockerContainer(
+				    try {
+
+					    docker.withRegistry( config.dockerRegistryUrl, config.registryCredentialsId) {
+                            def buildAgent = docker.image(config.agentImage)
+						    def nodeMemory = config.nodeMemory ?: "8192"
+                            buildAgent.pull()
+                            withDockerContainer(
                                 args: "--net=host -u root -v /var/run/docker.sock:/var/run/docker.sock -e NODE_OPTIONS='--max-old-space-size=${nodeMemory}'",
                                 image: config.agentImage
-                        ){
-                            body()
-                        }
+                            ){
 
-                    }
+							    body()
+
+                            }
+                        }	
+
+					} catch (e) {
+
+					    echo "build failed"
+
+					} finally {
+
+					    post
+
+					}
                 }
             }
         }
