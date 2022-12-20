41bd9ddb1b468d476cc2b710987adbca17c92e55
Michael Fleming <michael.fleming@nih.gov>
2022-12-20 12:24:28 -0500

    updated debug

--- vars/runPostJobs.groovy
+++ vars/runPostJobs.groovy
@@ -6,6 +6,7 @@ def call(Map config){
 		def paramList = []
 		config.jobParams.each { paramList.add( it ) }
 		echo "${paramList}"
+		echo "${config.jobParams}"
 		
 		build job: "${config.jobPath}", parameters: paramList
 
