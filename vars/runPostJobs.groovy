86e7832771655aaa74ce22ce0f7ed3a604ba089a
Michael Fleming <michael.fleming@nih.gov>
2022-12-20 12:06:38 -0500

    updated params passed to post job

--- vars/runPostJobs.groovy
+++ vars/runPostJobs.groovy
@@ -3,10 +3,10 @@ def call(Map config){
 
 		echo "Running post-build job:  ${config.jobPath}"
 		
-		//def paramList = []
-		//config.jobParams.each { paramList.add(${it}) }
-		config.jobParams.each { echo it }
-		//echo "${paramList}"
+		def paramList = []
+		config.jobParams.each { paramList.add( it ) }
+		//config.jobParams.each { echo "${it}" }
+		echo "${paramList}"
 		
 		build job: "${config.jobPath}", parameters: [config.jobParams]
 
