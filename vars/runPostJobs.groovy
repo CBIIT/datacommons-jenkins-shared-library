c9d17df9b5d3ade654cce0f643f75046742036df
Michael Fleming <michael.fleming@nih.gov>
2022-12-20 12:11:05 -0500

    updated post function call

--- vars/runPostJobs.groovy
+++ vars/runPostJobs.groovy
@@ -5,10 +5,9 @@ def call(Map config){
 		
 		def paramList = []
 		config.jobParams.each { paramList.add( it ) }
-		//config.jobParams.each { echo "${it}" }
 		echo "${paramList}"
 		
-		build job: "${config.jobPath}", parameters: [config.jobParams]
+		build job: "${config.jobPath}", parameters: paramList
 
     }
 }
