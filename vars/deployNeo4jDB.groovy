0daa40c7f61ad11da5b00cbcdea7e5d9af54574c
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-01-22 15:51:07 -0500

    fixing bento-files neo4j_uri

--- vars/deployNeo4jDB.groovy
+++ vars/deployNeo4jDB.groovy
@@ -11,12 +11,14 @@ def call(Map config=[:]){
             dockerRegistryUrl: "${config.dockerRegistryUrl}",
             registryCredentialsId: "${config.registryCredentialsId}"
     ) {
+
         deployProperties parameterName: "${config.parameterName}", repoName: "${config.repoName}"
         if (config.playbookRepoUrl){
             gitCheckout checkoutDirectory: "playbooks", gitUrl: config.playbookRepoUrl, gitBranch: config.playbookRepoBranch
         }else{
             gitCheckout checkoutDirectory: "playbooks", gitUrl: "https://github.com/CBIIT/icdc-devops", gitBranch: "master"
         }
+        setEnvValues()
         stage("deploy"){
             if(parsedVars){
                 runAnsible playbook: "${WORKSPACE}/playbooks/${config.playbook}", inventory: "${WORKSPACE}/playbooks/${config.inventory}", tier: "${config.tier}", projectName: "${config.projectName}", extraAnsibleVars: parsedVars
