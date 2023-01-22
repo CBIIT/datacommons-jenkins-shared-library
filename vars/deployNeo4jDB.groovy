3ea307de919dce86da545d9c45b57ab0f1fe00aa
cloudprofessionals <mycloudprofessionals@gmail.com>
2023-01-22 00:11:07 -0500

    adding neo4j workflow

---
+++ vars/deployNeo4jDB.groovy
@@ -0,0 +1,29 @@
+def call(Map config=[:]){
+
+    def parsedVars = [:]
+    if (config.extraAnsibleVars != null){
+        parsedVars = config.extraAnsibleVars
+    }
+    deployStage(
+            label: "${config.label}",
+            useDockerAgent: "${config.useDockerAgent}",
+            agentImage: "${config.agentImage}",
+            dockerRegistryUrl: "${config.dockerRegistryUrl}",
+            registryCredentialsId: "${config.registryCredentialsId}"
+    ) {
+        deployNeo4jProperties()
+        if (config.playbookRepoUrl){
+            gitCheckout checkoutDirectory: "playbooks", gitUrl: config.playbookRepoUrl, gitBranch: config.playbookRepoBranch
+        }else{
+            gitCheckout checkoutDirectory: "playbooks", gitUrl: "https://github.com/CBIIT/icdc-devops", gitBranch: "master"
+        }
+        stage("deploy"){
+            if(parsedVars){
+                runAnsible playbook: "${WORKSPACE}/playbooks/${config.playbook}", inventory: "${WORKSPACE}/playbooks/${config.inventory}", tier: "${config.tier}", projectName: "${config.projectName}", extraAnsibleVars: parsedVars
+            }else{
+                runAnsible playbook: "${WORKSPACE}/playbooks/${config.playbook}", inventory: "${WORKSPACE}/playbooks/${config.inventory}", tier: "${config.tier}", projectName: "${config.projectName}"
+            }
+        }
+        notify secretPath: "${config.slackSecretPath}", secretName: "${config.slackSecretName}"
+    }
+}
