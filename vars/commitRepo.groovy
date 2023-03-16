fa8a3b0397f44d3baa9c4ddddf8aa1a75fa59e5b
Sowmya Karavadi <ksowmya442@gmail.com>
2023-03-16 12:58:05 -0400

    updated env to GIT_REPO_URL from GIT_URL

--- vars/commitRepo.groovy
+++ vars/commitRepo.groovy
@@ -1,6 +1,6 @@
 def call(Map config) {
     env.GIT_TOKEN = sh(script: 'aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token | tr -d \'\\n\'', returnStdout: true)
-    env.GIT_URL = "${config.gitUrl}".replace('https://','')
+    env.GIT_REPO_URL = "${config.gitUrl}".replace('https://','')
     def targetDirectory = ""
     if ("${config.checkoutDirectory}" == "workspace"){
         targetDirectory = "."
@@ -15,8 +15,8 @@ def call(Map config) {
         git config user.name "Bento Jenkins"
         git add --all .
         git commit -am "tagging latest deployment.yaml"
-        git push "https://${GIT_TOKEN}:x-oauth-basic@${GIT_URL}" HEAD:${params.Environment}
+        git push "https://${GIT_TOKEN}:x-oauth-basic@${GIT_REPO_URL}" HEAD:${params.Environment}
         git tag --no-sign -a "${config.service}.${BUILD_NUMBER}" -m "Jenkins tag: ${config.service}.${BUILD_NUMBER}"
-        git push "https://${GIT_TOKEN}:x-oauth-basic@${GIT_URL}" --tags
+        git push "https://${GIT_TOKEN}:x-oauth-basic@${GIT_REPO_URL}" --tags
 """
 }
