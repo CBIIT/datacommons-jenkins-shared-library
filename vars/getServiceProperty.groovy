b49b2e3298904ae77c932738711f439add9caa82
cloudprofessionals <mycloudprofessionals@gmail.com>
2024-01-31 21:45:39 -0500

    more tests

--- vars/getServiceProperty.groovy
+++ vars/getServiceProperty.groovy
@@ -2,9 +2,11 @@ def call(Map config = [:]){
     def deployment = readYaml file: config.deploymentFile
     def propertyValue = ""
     def propertyKey = config.property
+    println propertyKey
     deployment.services.each {
         if (it.key == config.service) {
             propertyValue = it.value."${propertyKey}"
+            println propertyValue
         }
     }
     return propertyValue
