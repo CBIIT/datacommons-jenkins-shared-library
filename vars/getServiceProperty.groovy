e7882968bd8fd8c37fa51ee9238f5bbcc22b33c7
cloudprofessionals <mycloudprofessionals@gmail.com>
2024-02-01 09:45:12 -0500

    removed print info

--- vars/getServiceProperty.groovy
+++ vars/getServiceProperty.groovy
@@ -2,11 +2,9 @@ def call(Map config = [:]){
     def deployment = readYaml file: config.deploymentFile
     def propertyValue = ""
     def propertyKey = config.property
-    println propertyKey
     deployment.services.each {
         if (it.key == config.service) {
             propertyValue = it.value."${propertyKey}"
-            println propertyValue
         }
     }
     return propertyValue
