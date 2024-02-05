59ebfea237c3142b04b0e3e1129b524a51e655f6
cloudprofessionals <mycloudprofessionals@gmail.com>
2024-02-05 16:19:31 -0500

    modified getservice property

--- vars/getServiceProperty.groovy
+++ vars/getServiceProperty.groovy
@@ -4,7 +4,13 @@ def call(Map config = [:]){
     def propertyKey = config.property
     deployment.services.each {
         if (it.key == config.service) {
+            // Check if the property exists in the main properties
             propertyValue = it.value."${propertyKey}"
+            
+            // If not found, check in additionalProperties
+            if (propertyValue == null && it.value.additionalProperties?.containsKey(propertyKey)) {
+                propertyValue = it.value.additionalProperties[propertyKey]
+            }
         }
     }
     return propertyValue
