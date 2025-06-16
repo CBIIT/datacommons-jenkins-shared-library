66dd433f39ab96075d69a623e3b1075a009a17bd
cloudprofessionals <mycloudprofessionals@gmail.com>
2025-06-16 12:24:32 -0400

    add user tag option

---
+++ vars/userTagRepo.groovy
@@ -0,0 +1,19 @@
+def call(Map config = [:]) {
+        //env.GIT_TOKEN = sh(script: 'aws secretsmanager get-secret-value  --secret-id github/pat --region us-east-1 | jq --raw-output .SecretString | jq -r .token | tr -d \'\\n\'', returnStdout: true)
+        env.GIT_TOKEN = "${config.gitToken}"
+        env.GIT_REPO_URL = "${config.gitUrl}".replace('https://','')
+        def targetDirectory = ""
+        if ("${config.checkoutDirectory}" == "workspace"){
+                targetDirectory = "."
+        }else{
+                targetDirectory = "${WORKSPACE}/${config.checkoutDirectory}"
+        }
+        sh """
+        cd "${targetDirectory}"
+        echo "Applying tag ${config.gitTag} to ${config.gitUrl}"
+        git config user.email "NCI-CTOS-DEVOPS-SVC@mail.nih.gov "
+        git config user.name "NCI-CTOS-DEVOPS-SVC"
+        git tag --no-sign -a "${config.gitTag}.${BUILD_NUMBER}" -m "Jenkins tag: ${config.gitTag}.${BUILD_NUMBER}"
+        git push "https://${GIT_TOKEN}:x-oauth-basic@${GIT_REPO_URL}" --tags
+"""
+}
